5b71c51e6fdcb68fb5984c5f49a59546caa0bbbd
Anan1754 <55744437+Anan1754@users.noreply.github.com>
2019-10-09 14:38:23 +0700

    Update and rename bot.php to botpush.php

---
+++ botpush.php
@@ -0,0 +1,19 @@
+<?php
+
+require "vendor/autoload.php";
+
+$access_token = 'b3/+WEjX81cnWIUiw+czfqAcQAwKPGMJfn+eI8QueUiqoUMaAVEwt26rYvySnnnoejgrVXhf+U4NTfJHA+hj7x1kxGytAmevvxzJBrF+t5iCqWWQn6fUSWWuwXmIW4is/Qu2py4xLe/KHa98noRjMgdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = '3f1ff554c7430563431ae020fff9966e';
+
+$pushID = '1622844346';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+
+$response = $bot->pushMessage($pushID, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
